7bf826dac4b076ebd0a6a40b8efe9a963a67ee97
stackprobe <stackprobes@gmail.com>
2018-07-03 09:23:45 +0900

    daily

--- WHTT/WHTT/PortNoWin.Designer.cs
+++ WHTT/WHTT/PortNoWin.Designer.cs
@@ -34,11 +34,13 @@
 			// 
 			// PortNo
 			// 
+			this.PortNo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
 			this.PortNo.Font = new System.Drawing.Font("メイリオ", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
 			this.PortNo.Location = new System.Drawing.Point(12, 12);
 			this.PortNo.MaxLength = 5;
 			this.PortNo.Name = "PortNo";
-			this.PortNo.Size = new System.Drawing.Size(204, 36);
+			this.PortNo.Size = new System.Drawing.Size(216, 36);
 			this.PortNo.TabIndex = 0;
 			this.PortNo.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
 			this.PortNo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PortNo_KeyPress);
@@ -47,7 +49,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(228, 63);
+			this.ClientSize = new System.Drawing.Size(240, 67);
 			this.Controls.Add(this.PortNo);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
