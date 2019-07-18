19db2cb431167d1bb99d68589029cae0945c61dd
stackprobe <stackprobes@gmail.com>
2019-07-19 08:25:08 +0900

    Melody

--- WHTT/WHTT/SaveLoadDialogs.cs
+++ WHTT/WHTT/SaveLoadDialogs.cs
@@ -14,6 +14,53 @@ namespace WHTT
 
 	public class SaveLoadDialogs
 	{
+		public static string SaveFile(string title, string filterString, string initialFile)
+		{
+			// filterString == 拡張子1.拡張子2.拡張子3 ...
+
+			string[] exts = filterString.Split('.');
+
+			int filterIndex = exts.Select(ext => "." + ext.ToLower()).ToList().IndexOf(Path.GetExtension(initialFile).ToLower());
+			filterIndex += exts.Length + 1;
+			filterIndex %= exts.Length + 1;
+			filterIndex++;
+
+			return SaveFile(title, filterString, Path.GetDirectoryName(initialFile), Path.GetFileName(initialFile), dlg => dlg.FilterIndex = filterIndex);
+		}
+
+		public static string LoadFile(string title, string filterString, string initialFile)
+		{
+			// filterString == [総称:]拡張子1.拡張子2.拡張子3 ...
+
+			string genericName = null;
+
+			{
+				int p = filterString.IndexOf(':');
+
+				if (p != -1)
+				{
+					genericName = filterString.Substring(0, p);
+					filterString = filterString.Substring(p + 1);
+				}
+			}
+
+			string[] exts = filterString.Split('.');
+
+			if (genericName == null)
+				genericName = string.Join("_", exts);
+
+			filterString = string.Join(";", filterString.Split('.').Select(ext => "*." + ext));
+			filterString = genericName + "ファイル(" + filterString + ")|" + filterString + "|すべてのファイル(*.*)|*.*";
+
+			int filterIndex = exts.Select(ext => "." + ext.ToLower()).Contains(Path.GetExtension(initialFile).ToLower()) ? 1 : 2;
+
+			return LoadFile(title, "", Path.GetDirectoryName(initialFile), Path.GetFileName(initialFile), dlg =>
+			{
+				dlg.Filter = filterString;
+				dlg.FilterIndex = filterIndex;
+			});
+		}
+
 		public static string SaveFile(string title, string filterString, string initialDir, string initialFile, Action<FileDialog> initializer = null)
 		{
 			return SaveLoadFile(title, filterString, initialDir, initialFile, initializer, true);
@@ -54,13 +101,15 @@ namespace WHTT
 			return null;
 		}
 
-		private static int GF_FilterIndex;
+		//private static int GF_FilterIndex;
 
 		private static string GetFilter(string filterString)
 		{
+			// filterString == [名前1:]拡張子1.[名前2:]拡張子2.[名前3:]拡張子3 ...
+
 			StringBuilder buff = new StringBuilder();
 
-			GF_FilterIndex = 1;
+			//GF_FilterIndex = 1;
 
 			foreach (string fExtension in filterString.Split('.').Select(extension => extension.Trim()).Where(extension => extension != ""))
 			{
@@ -77,7 +126,7 @@ namespace WHTT
 					}
 				}
 
-				GF_FilterIndex++;
+				//GF_FilterIndex++;
 
 				buff.Append(name + "ファイル(*." + extension + ")|*." + extension + "|");
 			}
