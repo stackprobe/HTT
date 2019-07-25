76fad40ec364acae0d22e78163b4373f0fc9985c
stackprobe <stackprobes@gmail.com>
2019-07-25 21:24:24 +0900

    Amour

--- WHTT/WHTT/Tools.cs
+++ WHTT/WHTT/Tools.cs
@@ -164,7 +164,7 @@ namespace WHTT
 
 		// sync > @ PostShown
 
-		public static void PostShown(Form f)
+		public static void PostShown_GetAllControl(Form f, Action<Control> reaction)
 		{
 			List<Control.ControlCollection> controlTable = new List<Control.ControlCollection>();
 
@@ -196,26 +196,45 @@ namespace WHTT
 						controlTable.Add(sc.Panel1.Controls);
 						controlTable.Add(sc.Panel2.Controls);
 					}
-					TextBox tb = control as TextBox;
+					Panel p = control as Panel;
 
-					if (tb != null)
+					if (p != null)
 					{
-						if (tb.ContextMenuStrip == null)
-						{
-							ToolStripMenuItem item = new ToolStripMenuItem();
-
-							item.Text = "項目なし";
-							item.Enabled = false;
-
-							ContextMenuStrip menu = new ContextMenuStrip();
-
-							menu.Items.Add(item);
-
-							tb.ContextMenuStrip = menu;
-						}
+						controlTable.Add(p.Controls);
 					}
+					reaction(control);
 				}
 			}
+		}
+
+		public static void PostShown(Form f)
+		{
+			PostShown_GetAllControl(f, control =>
+			{
+				Control c = new Control[]
+				{
+					control as TextBox,
+					control as NumericUpDown,
+				}
+				.FirstOrDefault(v => v != null);
+
+				if (c != null)
+				{
+					if (c.ContextMenuStrip == null)
+					{
+						ToolStripMenuItem item = new ToolStripMenuItem();
+
+						item.Text = "項目なし";
+						item.Enabled = false;
+
+						ContextMenuStrip menu = new ContextMenuStrip();
+
+						menu.Items.Add(item);
+
+						c.ContextMenuStrip = menu;
+					}
+				}
+			});
 		}
 
 		// < sync
