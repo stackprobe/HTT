2d6240c47ae29f2022e1130ca441c26df0bb5ec2
stackprobe <stackprobes@gmail.com>
2020-05-10 01:38:57 +0900

    Tender

--- HTT/HTT/Kernel.cpp
+++ HTT/HTT/Kernel.cpp
@@ -25,24 +25,61 @@ void updateMemory(void)
 	lastExVirtualFree = (uint64)ms.ullAvailExtendedVirtual;
 }
 
+// sync > @ updateDiskSpace
+
+uint64 lastDiskFree_User; // このプロセスが使用出来る空き領域サイズ
 uint64 lastDiskFree;
-uint64 lastDiskFree_real;
 uint64 lastDiskSize;
 
-void updateDiskSpace_Dir(char *dir)
+static void S_UpdateDiskSpace(char *dir)
 {
 	ULARGE_INTEGER a;
 	ULARGE_INTEGER f;
 	ULARGE_INTEGER t;
 
-	errorCase(m_isEmpty(dir));
-
 	/*
 		ドライブが存在しない || 準備出来ていない || ディレクトリが存在しない -> 失敗する。
 	*/
-	errorCase(!GetDiskFreeSpaceEx((LPCTSTR)dir, &a, &t, &f)); // ? 失敗
-
-	lastDiskFree      = (uint64)a.LowPart | (uint64)a.HighPart << 32;
-	lastDiskFree_real = (uint64)f.LowPart | (uint64)f.HighPart << 32;
+	if (!(int)GetDiskFreeSpaceEx((LPCTSTR)dir, &a, &t, &f)) // ? 失敗
+	{
+		error();
+	}
+	lastDiskFree_User = (uint64)a.LowPart | (uint64)a.HighPart << 32;
+	lastDiskFree      = (uint64)f.LowPart | (uint64)f.HighPart << 32;
 	lastDiskSize      = (uint64)t.LowPart | (uint64)t.HighPart << 32;
 }
+void updateDiskSpace(int drive)
+{
+	char dir[4];
+
+	dir[0] = drive;
+	dir[1] = ':';
+	dir[2] = '\\';
+	dir[3] = '\0';
+
+	S_UpdateDiskSpace(dir);
+}
+void updateDiskSpace_Dir(char *dir)
+{
+#if 0 // zantei
+	return S_UpdateDiskSpace(dir);
+#else
+	int drive;
+
+errorCase(!*dir); // 2bs
+
+	if(dir[1] == ':')
+	{
+		drive = dir[0];
+	}
+	else
+	{
+		dir = getCwd();
+		drive = dir[0];
+		memFree(dir);
+	}
+	return updateDiskSpace(drive);
+#endif
+}
+
+// < sync
--- HTT/HTT/Kernel.h
+++ HTT/HTT/Kernel.h
@@ -9,8 +9,13 @@ extern uint64 lastExVirtualFree;
 
 void updateMemory(void);
 
+// sync > @ updateDiskSpace_h
+
+extern uint64 lastDiskFree_User; // このプロセスが使用出来る空き領域サイズ
 extern uint64 lastDiskFree;
-extern uint64 lastDiskFree_real;
 extern uint64 lastDiskSize;
 
+void updateDiskSpace(int drive);
 void updateDiskSpace_Dir(char *dir);
+
+// < sync
